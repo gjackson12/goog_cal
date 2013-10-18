56465c60e153db303da5ccac094d43f82c1fef8d
Daniel McCoy <dan.m.mccoy@gmail.com>
2013-10-17 22:40:41 -0400

    redirect uri changed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
     access_type: 'offline',
     scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
-    redirect_uri:'http://localhost:3000/auth/google_oauth2/callback'
+    redirect_uri:'http://goog-cal.herokuapp.com/auth/google_oauth2/callback'
   }
 end
 
